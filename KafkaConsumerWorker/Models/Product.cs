b53438a22645c6a74f5c3b158c9b8bf447ac9dd9
Lê Khắc Thiện <muzankibut112@gmail.com>
2024-09-30 16:55:12 +0700

    Fix: Formating code

--- KafkaConsumerWorker/Models/Product.cs
+++ KafkaConsumerWorker/Models/Product.cs
@@ -6,9 +6,9 @@ namespace KafkaConsumerWorker.Models
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int? Id { get; set; }
-        public string? Name { get; set; }
-        public decimal? Price { get; set; }
-        public string? Description { get; set; }
+        public int?         Id              { get; set; }
+        public string?      Name            { get; set; }
+        public decimal?     Price           { get; set; }
+        public string?      Description     { get; set; }
     }
 }
--- KafkaConsumerWorker/Services/KafkaConsumerService.cs
+++ KafkaConsumerWorker/Services/KafkaConsumerService.cs
@@ -4,25 +4,25 @@ namespace KafkaConsumerWorker.Services
 {
     public class KafkaConsumerService : BackgroundService
     {
-        private readonly IServiceProvider _serviceProvider;
-        private readonly string _bootstrapServers;
-        private readonly string _topic;
+        private readonly IServiceProvider   _serviceProvider;
+        private readonly string             _bootstrapServers;
+        private readonly string             _topic;
 
         public KafkaConsumerService(IServiceProvider serviceProvider, string bootstrapServers, string topic)
         {
-            _serviceProvider = serviceProvider;
-            _bootstrapServers = bootstrapServers;
-            _topic = topic;
+            _serviceProvider    = serviceProvider;
+            _bootstrapServers   = bootstrapServers;
+            _topic              = topic;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             var config = new ConsumerConfig
             {
-                GroupId = "product-consumer-group-1",
-                BootstrapServers = _bootstrapServers,
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                AllowAutoCreateTopics = true
+                GroupId                 = "product-consumer-group-1",
+                BootstrapServers        = _bootstrapServers,
+                AutoOffsetReset         = AutoOffsetReset.Earliest,
+                AllowAutoCreateTopics   = true
             };
 
             using (var consumer = new ConsumerBuilder<Null, string>(config).Build())
@@ -33,15 +33,15 @@ namespace KafkaConsumerWorker.Services
                 {
                     while (!stoppingToken.IsCancellationRequested)
                     {
-                        var consumeResult = consumer.Consume(stoppingToken);
-                        var productJson = consumeResult.Message.Value;
+                        var consumeResult   = consumer.Consume(stoppingToken);
+                        var productJson     = consumeResult.Message.Value;
 
-                        var product = JsonConvert.DeserializeObject<Product>(productJson);
-                        string productInfomation = $"[Product Name: {product.Name} - Price: {product.Price} - Description: {product.Description}]";
+                        var product                 = JsonConvert.DeserializeObject<Product>(productJson);
+                        string productInfomation    = $"[Product Name: {product.Name} - Price: {product.Price} - Description: {product.Description}]";
                         await Console.Out.WriteLineAsync($"Consumed product: {productInfomation} [ at {consumeResult.TopicPartition} ]");
 
-                        using var scope = _serviceProvider.CreateScope();
-                        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                        using var scope     = _serviceProvider.CreateScope();
+                        var dbContext       = scope.ServiceProvider.GetRequiredService<AppDbContext>();
                         dbContext.Products.Add(product);
                         await dbContext.SaveChangesAsync();
 
